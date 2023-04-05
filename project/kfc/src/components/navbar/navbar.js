52a587553df5ee5fb5c0ee489897557c6687632c
TinPham110902 <utru321@gmail.com>
2023-04-05 22:10:16 +0700

    add components

---
+++ project/kfc/src/components/navbar/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class navbar extends React.Component {
+    render() {
+        return (
+            <div>
+                navbar code
+            </div>
+        );
+    }
+}
+
+export default navbar;
