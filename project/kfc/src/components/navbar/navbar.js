79bad1239dbbb2bd502f88c4cd3391e1ca086c7e
hungnguyenquang221 <hungnguyenquang221@gmail.com>
2023-04-06 10:00:37 +0700

    change

--- project/kfc/src/components/navbar/navbar.js
+++ project/kfc/src/components/navbar/navbar.js
@@ -1,18 +1,24 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
-import "../../views/App.css"
+import React from "react";
+import "../../views/App.css";
 class Navbar extends React.Component {
-    render() {
-        return (
-            <div className='nav-bar'>
-                <nav class="nav justify-content-center">
-                    <a class="nav-link active" href="/">Home</a>
-                    <a class="nav-link" href="/ThemNv">ThemNV</a>
-                    <a class="nav-link disabled" href="#">Disabled link</a>
-                </nav>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="nav-bar">
+        <nav class="nav justify-content-center">
+          <a class="nav-link active" href="/">
+            Home
+          </a>
+          <a class="nav-link" href="/ThemNv">
+            ThemNV
+          </a>
+          <a class="nav-link disabled" href="#">
+            Disabled link
+          </a>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
--- project/kfc/src/views/App.js
+++ project/kfc/src/views/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from '../components/Navbar/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "../components/navbar/Navbar";
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
