2d163fc0d7999f873b3572f139e4e335d2a27184
hungnguyenquang221 <hungnguyenquang221@gmail.com>
2023-04-06 08:37:29 +0700

    change navbar.js to Navbar.js

--- project/kfc/src/views/App.js
+++ project/kfc/src/views/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from '../components/Navbar/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "../components/navbar/Navbar";
 function App() {
   return (
     <div className="App">
@@ -18,10 +18,8 @@ function App() {
         >
           Learn React
         </a>
-
       </header>
     </div>
-
   );
 }
 
