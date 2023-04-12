4b942d9daa6d53fe9c82bc5235418d7299b833e2
HuynhNgocAnh22 <128014952+HuynhNgocAnh22@users.noreply.github.com>
2023-04-12 09:46:00 +0700

    Grant Permission (Cấp quyền)

---
+++ FastFood/FastFood/Controllers/GrantPermissionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FastFood.Controllers
+{
+    public class GrantPermissionController : Controller
+    {
+        // GET: GrantPermission
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
